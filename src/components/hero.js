fc3414c256d4ed0105f41d24f2f5a0e2ba104846
Dhanny-aay <danielakintolu@gmail.com>
2024-03-04 13:19:36 +0100

    added animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,12 +10,12 @@ const Hero = () => {
             <Navbar/>
             <div className=' w-full pl-20 flex justify-between'>
                 <div className='w-[40%] mt-16'>
-                    <p className=' text-white text-6xl leading-none font-medium font-Outfit'>Providing Top Quality Service in your area.</p>
-                    <p className=' text-[#e8e8e8] text-sm absolute bottom-10 w-[300px] font-Outfit'>Clean Burst is a family owned pressure washing company in Reston VA. We are fully insured, licensed, and focused solely on providing you the best possible service.</p>
+                    <p className=' text-white text-6xl leading-none font-medium font-Outfit fade-in'>Providing Top Quality Service in your area.</p>
+                    <p className=' text-[#e8e8e8] text-sm absolute bottom-10 w-[300px] font-Outfit fade-in'>Clean Burst is a family owned pressure washing company in Reston VA. We are fully insured, licensed, and focused solely on providing you the best possible service.</p>
                 </div>
                 <div className='w-[50%] mt-16'>
-                    <p className=' text-[#e8e8e8] font-Outfit w-[300px]'>Brand New Hot Water Exterior Cleaning Service serving Fairfax and Loudoun County.</p>
-                    <div style={{backgroundImage:`url(${tima})`, backgroundPosition:'center', backgroundSize:'cover'}} className=' bg-[#f4f4f4] mt-10 w-full h-[450px]'></div>
+                    <p className=' text-[#e8e8e8] font-Outfit w-[300px] fade-in2'>Brand New Hot Water Exterior Cleaning Service serving Fairfax and Loudoun County.</p>
+                    <div style={{backgroundImage:`url(${tima})`, backgroundPosition:'center', backgroundSize:'cover'}} className=' bg-[#f4f4f4] mt-10 w-full h-[450px] scale-up-br'></div>
                 </div>
             </div>
         </div>
